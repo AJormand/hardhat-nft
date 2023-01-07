89eb418a1ebb8297e491d5260ddd0805ebba35ab
Aro <aro.jormand@gmail.com>
2023-01-07 20:47:40 +0100

    DynamicSvgNft contract

--- contracts/DynamicSvgNft.sol
+++ contracts/DynamicSvgNft.sol
@@ -19,9 +19,13 @@ contract DynamicSvgNft is ERC721 {
 
     constructor(string memory lowSvg, string memory highSvg) ERC721("Dynamic SVG NFT", "DSN"){
         s_tokenCounter = 0;
+        i_lowImageURI = svgToImageURI(lowSvg);
+        i_highImageURI = svgToImageURI(highSvg);
     }
 
     function svgToImageURI(string memory svg) public pure returns(string memory) {
+        //encoding svg to base64 so that it takes less space on chain
+        // '<svg width="500" height="500" viewBox="0 0 285 350" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill="black" d="M150,0,L75,200,L225,200,Z"></path></svg>'
         string memory svgBase64Encoded = Base64.encode(bytes(string(abi.encodePacked(svg))));
         return string(abi.encodePacked(base64EncodedSvgPrefix, svgBase64Encoded));
     }
@@ -29,6 +33,32 @@ contract DynamicSvgNft is ERC721 {
     function mintNft() public {
         _safeMint(msg.sender, s_tokenCounter);
         s_tokenCounter = s_tokenCounter +1;
+    }
+
+    function _baseURI() internal pure override returns(string memory) {
+        return "data:application/json;base64,";
+    }
+
+    function tokenURI(uint256 tokenId) public view virtual override returns(string memory){
+        require(_exists(tokenId), "URI query for nonexistent token");
+        string memory imageURI = "hi!";
+
+        //prefix for svg images --> data:image/svg+xml;base64
+        //prefix for data json -->  data:application/json;base64
+
+        return 
+        string(abi.encodePacked(
+            _baseURI(),
+            Base64.encode(
+                bytes(abi.encodePacked(
+                '{"name":', name(), '", "description":"An NFT that changes based on the Chainlink Feed", ',
+                '"attributes": [{"trait_type": "coolness", "value": 100}], "image":"', imageURI,
+                '"}'
+                    )
+                )
+            )
+            )
+        );
     }
 
 
